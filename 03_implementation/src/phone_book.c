3249827f73e6e21679bcc17ca04161f9371e89da
STEPin105112 <69114229+STEPin105112@users.noreply.github.com>
2020-10-09 20:55:44 +0530

    Update phone_book.c

--- 03_implementation/src/phone_book.c
+++ 03_implementation/src/phone_book.c
@@ -1,13 +1,12 @@
 #include "phone_book.h"
 int password()
 {
- int x=10;
- char pass[6];
+ int pass,x=10;
  while(x!=0)
  {
    printf("\nInput the password");
-   scanf("%s",pass);
-   if(pass=="1234")
+   scanf("%d",&pass);
+   if(pass==1234)
    {
      printf("correct password");
      x=0;
